dbfbf40d8985cb4be457df30993ec7a41c7a4c26
Dustin Holman <dustinsprk@gmail.com>
2022-02-17 13:35:59 -0800

    add hours flag to sd

--- sd/main.go
+++ sd/main.go
@@ -8,17 +8,25 @@ import (
 )
 
 var suppressNL bool
+var hours bool
 
 func init() {
 	flag.BoolVar(&suppressNL, "suppress-new-line", false, "suppress a final newline")
 	flag.BoolVar(&suppressNL, "s", false, "(abbrv) suppress a final newline")
+	flag.BoolVar(&hours, "hours", false, "show hours instead of days")
+	flag.BoolVar(&hours, "h", false, "(abbrv) show hours instead of days")
+
 	flag.Parse()
 }
 
 func main() {
 	now := time.Now().In(time.Local)
 	then := time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local)
-	r := now.Sub(then).Hours() / float64(24)
+	divisor := float64(24)
+	if hours {
+		divisor = float64(1)
+	}
+	r := now.Sub(then).Hours() / divisor
 	rem := math.Ceil(r)
 	suff := "\n"
 	if suppressNL {
