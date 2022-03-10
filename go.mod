3110d0a43553f91c45bc88cee2d9ccc71169c5dc
Dustin Holman <dustinsprk@gmail.com>
2022-03-10 12:16:35 -0800

    modernizing, add weeks/hours to sd

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dustinsprk/tools
+
+go 1.17
+
+require github.com/google/uuid v0.0.0-20161128191214-064e2069ce9c
--- sd/main.go
+++ sd/main.go
@@ -4,30 +4,41 @@ import (
 	"flag"
 	"fmt"
 	"math"
+	"os"
 	"time"
 )
 
 var suppressNL bool
 var hours bool
+var weeks bool
 
 func init() {
 	flag.BoolVar(&suppressNL, "suppress-new-line", false, "suppress a final newline")
 	flag.BoolVar(&suppressNL, "s", false, "(abbrv) suppress a final newline")
 	flag.BoolVar(&hours, "hours", false, "show hours instead of days")
-	flag.BoolVar(&hours, "h", false, "(abbrv) show hours instead of days")
-
+	flag.BoolVar(&hours, "H", false, "show hours instead of days")
+	flag.BoolVar(&weeks, "weeks", false, "(abbrv) show seeks instead of days")
+	flag.BoolVar(&weeks, "w", false, "(abbrv) show weeks instead of days")
 	flag.Parse()
 }
 
 func main() {
+	if hours && weeks {
+		fmt.Fprintln(os.Stderr, "hours and weeks cannot both be set")
+		os.Exit(1)
+	}
+
 	now := time.Now().In(time.Local)
 	then := time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local)
 	divisor := float64(24)
 	if hours {
 		divisor = float64(1)
 	}
+	if weeks {
+		divisor = float64(168)
+	}
 	r := now.Sub(then).Hours() / divisor
-	rem := math.Ceil(r)
+	rem := math.Round(r)
 	suff := "\n"
 	if suppressNL {
 		suff = ""
